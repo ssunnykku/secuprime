df530ff6352d1cffa0bc029916613bb9104b14f6
ssunnykku <ssunnykku@gmail.com>
2023-08-25 13:55:07 +0900

    fix: ecosystem.config

--- src/config/ecosystem.config.cjs
+++ src/config/ecosystem.config.cjs
@@ -1,26 +1,17 @@
 module.exports = {
   apps: [
     {
+      name: 'secuprime',
       script: '../index.js',
-      instances: 1,
-      autorestart: false,
-      watch: false,
+      instances: 'max',
       exec_mode: 'cluster',
+      watch: false,
       env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
         NODE_ENV: 'production',
       },
     },
   ],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'http://sunhee.duckdns.org/',
-      ref: 'origin/main',
-      repo: 'https://github.com/ssunnykku/secuprime.git',
-      path: '/home/ubuntu/secuprime',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'npm install && npm run build && pm2 reload ecosystem.config.js',
-    },
-  },
 };
