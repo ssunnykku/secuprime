52faddf7b41f8c8f1d73aefd11d8f700d65eaae2
ssunnykku <ssunnykku@gmail.com>
2023-08-25 14:09:42 +0900

    fix: ecosystem.config env 관련 삭제

--- src/config/ecosystem.config.cjs
+++ src/config/ecosystem.config.cjs
@@ -6,12 +6,6 @@ module.exports = {
       instances: 'max',
       exec_mode: 'cluster',
       watch: false,
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
