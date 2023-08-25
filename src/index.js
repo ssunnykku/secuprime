f097afac59e461b90457dd1baf1aad2988e0f345
ssunnykku <ssunnykku@gmail.com>
2023-08-25 11:08:38 +0900

    feat: ecosysyem.config.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'dotenv/config';
-
 import path, { dirname } from 'path'; // Added dirname import here
 import { fileURLToPath } from 'url';
 
@@ -21,12 +20,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
 
 //routes
 app.use(custRouter);
-// app.get('/', function (req, res) {
-//   res.send('Hello World');
-// });
+
 const port = process.env.SEVER_PORT;
 app.listen(port, console.log('OK'));
